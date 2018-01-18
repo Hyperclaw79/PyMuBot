3caaffecda964baa6cfcc67570f7c2ee53c8793c
Harshith Thota <harshith.thota7@gmail.com>
2018-01-18 21:09:58 +0530

    Delete secrets.py

--- microservices/app/src/secrets.py
+++
@@ -1,6 +0,0 @@
-consumer_key = 'hnEVn60chLDGTOidchkHhMC4v'
-consumer_secret = 'vw8ChzxSabc9oDBO83EHOMly5lXCC1ildyvx4Dm4zGropr3AnK'
-access_token = '951563504170893312-9oL7BcMQSHYmyebmN0j0MN7LCuaMyNW'
-access_secret = 'VfGtlTKmeoIdplSssldcTDwAvJsUcIFKsAMtqnZGpRHxe'
-MM_API_KEY = '32e9e1e41b07aa575dc4fa2a716e3582'
-
